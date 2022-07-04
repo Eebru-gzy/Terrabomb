a3f7bd4536a2665ccadba370872ecdc26e1850ee
Opeyemi Alao <alaoopeyemi101@gmail.com>
2022-07-04 12:14:40 +0100

    add ext vpc modules

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
 
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "my-vpc"
+  cidr = var.vpc_cidr_block
 
-resource "aws_vpc" "test-vpc" {
-  cidr_block = var.vpc_cidr_block
+  azs             = [var.avail_zone]
+  public_subnets  = [var.subnet_cidr_block]
+  public_subnet_tags = {
+    Name = "${var.env_prefix}-public-subnet"
+  }
+
   tags = {
-    Name = "${var.env_prefix}-vpc"
+    Name =  "${var.env_prefix}-vpc"
   }
 }
 
-module "local-subnet" {
-  source = "./modules/subnet"
-  subnet_cidr_block = var.subnet_cidr_block
-  avail_zone = var.avail_zone
-  env_prefix = var.env_prefix
-  vpc_id = aws_vpc.test-vpc.id
-  default_route_table_id = aws_vpc.test-vpc.default_route_table_id
-}
 
 module "webserver" {
   source = "./modules/webserver"
   public_key_location = var.public_key_location
   avail_zone = var.avail_zone
   env_prefix = var.env_prefix
-  vpc_id = aws_vpc.test-vpc.id
+  vpc_id = module.vpc.vpc_id
   instance_type = var.instance_type
-  subnet_id = module.local-subnet.subnet.id
+  subnet_id = module.vpc.public_subnets[0]
   my_ip = var.my_ip
   image_name = var.image_name
   user_data_location = var.user_data_location
